77c47bf8acba9ae7ac1c52c2c769b001cbc20e05
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-04-21 09:11:51 +0200

    Ajout de la base du code

---
+++ code/index.php
@@ -0,0 +1,5 @@
+<?php
+    session_start();
+    ob_start();
+    require "vue/template.php";
+?>
---
+++ code/vue/template.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/../style.css">
+    <title>Cinéma</title>
+</head>
+    <header>
+        <nav>
+            <h1>CINÉMA</h1>
+            <a href="#">
+                <button>FILMS</button>
+            </a>
+            <a href="#">
+                <button>ACTEURS</button>
+            </a>
+            <a href="#">
+                <button>RÉALISATEURS</button>
+            </a>
+            <a href="#">
+                <button>RÔLES</button>
+            </a>
+            <a href="#">
+                <button>GENRES</button>
+            </a>
+        </nav>
+    </header>
+    <body>
+        
+    </body>
+</html>
