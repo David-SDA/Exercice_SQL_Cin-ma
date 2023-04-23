42ed640f0d37d0422432e92ff424857a20232e61
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-04-23 10:24:02 +0200

    Ajout de l'action de lister les réalisateurs

--- app/index.php
+++ app/index.php
@@ -1,11 +1,13 @@
 <?php
     /* Ajout de tout les contrôleurs */
     require_once("controller/AccueilController.php");
+    require_once("controller/RealisateurController.php");
     require_once("controller/RoleController.php");
     require_once("controller/GenreController.php");
 
     /* Création des objets contrôleurs */
     $controleurAccueil = new AccueilController();
+    $controleurRealisateur = new RealisateurController();
     $controleurRole = new RoleController();
     $controleurGenre = new GenreController();
 
@@ -13,6 +15,11 @@
     if(isset($_GET['action'])){
         /* On effectue un switch sur celle-ci */
         switch($_GET['action']){
+            /* Action pour les réalisateurs */
+            case "listerRealisateurs": // lister les réalisateurs
+                $controleurRealisateur->listerRealisateurs();
+                break;
+
             /* Action pour les rôles */
             case "listerRoles": // lister les rôles
                 $controleurRole->listerRoles();
