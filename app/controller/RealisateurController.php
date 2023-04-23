cf433528ce03d2fa3822652c37df5378b8d107d6
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-04-23 10:25:27 +0200

    Ajout d'un controleur pour les réalisateurs

---
+++ app/controller/RealisateurController.php
@@ -0,0 +1,18 @@
+<?php
+    require_once("model/Model.php");
+
+    /* On crée un contrôleur pour gérer les action en rapport aux genres */
+    class RealisateurController{
+
+        public function listerRealisateurs(){
+            $donnee = new Model();
+            $requete = "SELECT p.prenom, p.nom, p.sexe, DATE_FORMAT(p.date_naissance, '%d/%m/%Y') AS date_naissance
+                        FROM personne p, realisateur r
+                        WHERE p.id_personne = r.id_personne";
+            $realisateurs = $donnee->executerRequete($requete);
+            require("view/Realisateur/viewListeRealisateur.php");
+        }
+
+    }
+
+?>
